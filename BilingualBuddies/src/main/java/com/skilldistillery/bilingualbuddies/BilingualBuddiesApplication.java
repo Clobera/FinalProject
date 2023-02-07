c88d6054146c7d348933c64f71332b0fb9aac634
Lowell Belany <lrbelany@gmail.com>
2023-02-07 09:57:19 -0800

    Stub Spring security configuration

--- BilingualBuddies/src/main/java/com/skilldistillery/bilingualbuddies/BilingualBuddiesApplication.java
+++ BilingualBuddies/src/main/java/com/skilldistillery/bilingualbuddies/BilingualBuddiesApplication.java
@@ -2,6 +2,9 @@ package com.skilldistillery.bilingualbuddies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class BilingualBuddiesApplication {
@@ -9,5 +12,8 @@ public class BilingualBuddiesApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BilingualBuddiesApplication.class, args);
 	}
-
+	@Bean
+	public PasswordEncoder configurePasswordEncoder() {
+	   return new BCryptPasswordEncoder();
+	}
 }
