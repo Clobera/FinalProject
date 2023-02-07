02bf4fe20d2457d3b4385b5e447bf4d3b1dac990
Lowell Belany <lrbelany@gmail.com>
2023-02-07 09:40:02 -0800

    Strubs for JPA and Spring Boot

---
+++ BilingualBuddies/src/main/java/com/skilldistillery/bilingualbuddies/BilingualBuddiesApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.bilingualbuddies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilingualBuddiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilingualBuddiesApplication.class, args);
+	}
+
+}
---
+++ BilingualBuddies/src/test/java/com/skilldistillery/bilingualbuddies/BilingualBuddiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.bilingualbuddies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilingualBuddiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
