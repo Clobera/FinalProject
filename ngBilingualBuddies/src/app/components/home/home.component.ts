ea0bf39ac95a926f87250f577adb6361ff9b549a
Carlos Lobera <carlosclobera@gmail.com>
2023-02-07 15:23:59 -0600

    Configure angular, bootstrap, and auth test

--- ngBilingualBuddies/src/app/components/home/home.component.ts
+++ ngBilingualBuddies/src/app/components/home/home.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+
+
+
+
+
+  //METHODS
+  constructor(private auth: AuthService){}
+
+  ngOnInit(){
+    this.tempTestDeleteMeLater(); //DELETE ME
+  }
+
+  tempTestDeleteMeLater(){
+    this.auth.login('admin', 'wombat1').subscribe({
+      next: (result) => {
+        console.log('LOGGED IN :D');
+        console.log(result);
+
+      },
+      error: (ugh) => {
+        console.error('ERROR AUTHENTICATING:');
+        console.error(ugh);
+
+      }
+    });
+  }
 
 }
