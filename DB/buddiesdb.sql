ef84b4e067bf7e24562e8b21b5baae7310f4de89
Sam Cassidy <samuelcassidy1999@gmail.com>
2023-02-07 16:08:19 -0700

    Finish database schema

--- DB/buddiesdb.sql
+++ DB/buddiesdb.sql
@@ -16,6 +16,36 @@ CREATE SCHEMA IF NOT EXISTS `bilingualbuddiesdb` DEFAULT CHARACTER SET utf8 ;
 USE `bilingualbuddiesdb` ;
 
 -- -----------------------------------------------------
+-- Table `address`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `address` ;
+
+CREATE TABLE IF NOT EXISTS `address` (
+  `id` INT NOT NULL,
+  `address` VARCHAR(50) NULL,
+  `addresscol` VARCHAR(45) NULL,
+  `city_id` INT NOT NULL,
+  `state` VARCHAR(50) NULL,
+  `postal_code` VARCHAR(10) NULL,
+  `city` VARCHAR(100) NULL,
+  `enabled` TINYINT NULL DEFAULT 1,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `country`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `country` ;
+
+CREATE TABLE IF NOT EXISTS `country` (
+  `country_code` CHAR(2) NOT NULL,
+  `country` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`country_code`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `user`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `user` ;
@@ -34,8 +64,22 @@ CREATE TABLE IF NOT EXISTS `user` (
   `last_name` VARCHAR(50) NOT NULL,
   `image_url` TEXT NULL,
   `bio` TEXT NULL,
+  `address_id` INT NULL,
+  `origin_country` CHAR(2) NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE INDEX `username_UNIQUE` (`username` ASC))
+  UNIQUE INDEX `username_UNIQUE` (`username` ASC),
+  INDEX `fk_user_address1_idx` (`address_id` ASC),
+  INDEX `fk_user_country1_idx` (`origin_country` ASC),
+  CONSTRAINT `fk_user_address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `address` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_user_country1`
+    FOREIGN KEY (`origin_country`)
+    REFERENCES `country` (`country_code`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -49,6 +93,8 @@ CREATE TABLE IF NOT EXISTS `post` (
   `user_id` INT NOT NULL,
   `content` TEXT NOT NULL,
   `image_url` TEXT NULL,
+  `post_date` DATETIME NULL,
+  `enabled` TINYINT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
   INDEX `fk_post_user1_idx` (`user_id` ASC),
   CONSTRAINT `fk_post_user1`
@@ -60,18 +106,97 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `notification`
+-- Table `team`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `notification` ;
+DROP TABLE IF EXISTS `team` ;
 
-CREATE TABLE IF NOT EXISTS `notification` (
+CREATE TABLE IF NOT EXISTS `team` (
   `id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  `content` VARCHAR(200) NULL,
+  `owner_id` INT NOT NULL,
+  `content` TEXT NULL,
+  `image_url` TEXT NULL,
+  `name` VARCHAR(100) NULL,
+  `create_date` DATETIME NULL,
+  `enabled` TINYINT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
-  CONSTRAINT `fk_notification_user`
-    FOREIGN KEY (`user_id`)
+  INDEX `fk_group_user1_idx` (`owner_id` ASC),
+  CONSTRAINT `fk_group_user1`
+    FOREIGN KEY (`owner_id`)
     REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `meetup`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `meetup` ;
+
+CREATE TABLE IF NOT EXISTS `meetup` (
+  `id` INT NOT NULL,
+  `meetup_date` DATE NOT NULL,
+  `content` TEXT NULL,
+  `address_id` INT NOT NULL,
+  `enabled` TINYINT NULL DEFAULT 1,
+  `owner_id` INT NOT NULL,
+  `team_id` INT NULL,
+  `start_time` TIME NULL,
+  `end_time` TIME NULL,
+  `created_date` DATETIME NULL,
+  `image_url` TEXT NULL,
+  `title` VARCHAR(150) NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_event_address1_idx` (`address_id` ASC),
+  INDEX `fk_event_user1_idx` (`owner_id` ASC),
+  INDEX `fk_event_group1_idx` (`team_id` ASC),
+  CONSTRAINT `fk_event_address1`
+    FOREIGN KEY (`address_id`)
+    REFERENCES `address` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_event_user1`
+    FOREIGN KEY (`owner_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_event_group1`
+    FOREIGN KEY (`team_id`)
+    REFERENCES `team` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `alert`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `alert` ;
+
+CREATE TABLE IF NOT EXISTS `alert` (
+  `id` INT NOT NULL,
+  `sender_id` INT NOT NULL,
+  `content` VARCHAR(200) NULL,
+  `receiver_id` INT NOT NULL,
+  `meetup_id` INT NULL,
+  `notification_date` DATETIME NOT NULL,
+  `seen` TINYINT NULL DEFAULT 0,
+  PRIMARY KEY (`id`),
+  INDEX `fk_notification_user1_idx` (`receiver_id` ASC),
+  INDEX `fk_notification_meetup1_idx` (`meetup_id` ASC),
+  CONSTRAINT `fk_notification_user`
+    FOREIGN KEY (`sender_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_notification_user1`
+    FOREIGN KEY (`receiver_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_notification_meetup1`
+    FOREIGN KEY (`meetup_id`)
+    REFERENCES `meetup` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -85,30 +210,19 @@ DROP TABLE IF EXISTS `comment` ;
 CREATE TABLE IF NOT EXISTS `comment` (
   `id` INT NOT NULL,
   `post_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
   `content` TEXT NOT NULL,
+  `comment_date` DATETIME NULL,
+  `enabled` TINYINT NULL DEFAULT 1,
   PRIMARY KEY (`id`),
+  INDEX `fk_comment_user1_idx` (`user_id` ASC),
   CONSTRAINT `fk_comment_post1`
     FOREIGN KEY (`post_id`)
     REFERENCES `post` (`id`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `group`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `group` ;
-
-CREATE TABLE IF NOT EXISTS `group` (
-  `id` INT NOT NULL,
-  `owner_id` INT NOT NULL,
-  `content` TEXT NULL,
-  `image_url` TEXT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_group_user1_idx` (`owner_id` ASC),
-  CONSTRAINT `fk_group_user1`
-    FOREIGN KEY (`owner_id`)
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_comment_user1`
+    FOREIGN KEY (`user_id`)
     REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
@@ -116,11 +230,11 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `group_has_member`
+-- Table `team_has_member`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `group_has_member` ;
+DROP TABLE IF EXISTS `team_has_member` ;
 
-CREATE TABLE IF NOT EXISTS `group_has_member` (
+CREATE TABLE IF NOT EXISTS `team_has_member` (
   `group_id` INT NOT NULL,
   `user_id` INT NOT NULL,
   PRIMARY KEY (`group_id`, `user_id`),
@@ -128,92 +242,12 @@ CREATE TABLE IF NOT EXISTS `group_has_member` (
   INDEX `fk_group_has_user_group1_idx` (`group_id` ASC),
   CONSTRAINT `fk_group_has_user_group1`
     FOREIGN KEY (`group_id`)
-    REFERENCES `group` (`id`)
+    REFERENCES `team` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_group_has_user_user1`
     FOREIGN KEY (`user_id`)
     REFERENCES `user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `country`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `country` ;
-
-CREATE TABLE IF NOT EXISTS `country` (
-  `country_code` CHAR(2) NOT NULL,
-  `country` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`country_code`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `city`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `city` ;
-
-CREATE TABLE IF NOT EXISTS `city` (
-  `id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  `name` VARCHAR(200) NOT NULL,
-  `country_country_code` CHAR(2) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_city_user1_idx` (`user_id` ASC),
-  INDEX `fk_city_country1_idx` (`country_country_code` ASC),
-  CONSTRAINT `fk_city_user1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_city_country1`
-    FOREIGN KEY (`country_country_code`)
-    REFERENCES `country` (`country_code`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `address`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `address` ;
-
-CREATE TABLE IF NOT EXISTS `address` (
-  `id` INT NOT NULL,
-  `address` VARCHAR(50) NULL,
-  `addresscol` VARCHAR(45) NULL,
-  `city_id` INT NOT NULL,
-  `state` VARCHAR(50) NULL,
-  `postal_code` VARCHAR(10) NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_address_city1_idx` (`city_id` ASC),
-  CONSTRAINT `fk_address_city1`
-    FOREIGN KEY (`city_id`)
-    REFERENCES `city` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `event`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `event` ;
-
-CREATE TABLE IF NOT EXISTS `event` (
-  `id` INT NOT NULL,
-  `date` DATETIME NOT NULL,
-  `content` TEXT NULL,
-  `address_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_event_address1_idx` (`address_id` ASC),
-  CONSTRAINT `fk_event_address1`
-    FOREIGN KEY (`address_id`)
-    REFERENCES `address` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -226,24 +260,18 @@ DROP TABLE IF EXISTS `language` ;
 
 CREATE TABLE IF NOT EXISTS `language` (
   `id` INT NOT NULL,
-  `user_id` INT NOT NULL,
   `name` VARCHAR(100) NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_language_user1_idx` (`user_id` ASC),
-  CONSTRAINT `fk_language_user1`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  `description` TEXT NULL,
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `user_has_event`
+-- Table `user_has_meetup`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `user_has_event` ;
+DROP TABLE IF EXISTS `user_has_meetup` ;
 
-CREATE TABLE IF NOT EXISTS `user_has_event` (
+CREATE TABLE IF NOT EXISTS `user_has_meetup` (
   `user_id` INT NOT NULL,
   `event_id` INT NOT NULL,
   PRIMARY KEY (`user_id`, `event_id`),
@@ -256,7 +284,84 @@ CREATE TABLE IF NOT EXISTS `user_has_event` (
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_user_has_event_event1`
     FOREIGN KEY (`event_id`)
-    REFERENCES `event` (`id`)
+    REFERENCES `meetup` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `language_has_user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `language_has_user` ;
+
+CREATE TABLE IF NOT EXISTS `language_has_user` (
+  `language_id` INT NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`language_id`, `user_id`),
+  INDEX `fk_language_has_user_user1_idx` (`user_id` ASC),
+  INDEX `fk_language_has_user_language1_idx` (`language_id` ASC),
+  CONSTRAINT `fk_language_has_user_language1`
+    FOREIGN KEY (`language_id`)
+    REFERENCES `language` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_language_has_user_user1`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `user` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `country_has_language`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `country_has_language` ;
+
+CREATE TABLE IF NOT EXISTS `country_has_language` (
+  `country_country_code` CHAR(2) NOT NULL,
+  `language_id` INT NOT NULL,
+  PRIMARY KEY (`country_country_code`, `language_id`),
+  INDEX `fk_country_has_language_language1_idx` (`language_id` ASC),
+  INDEX `fk_country_has_language_country1_idx` (`country_country_code` ASC),
+  CONSTRAINT `fk_country_has_language_country1`
+    FOREIGN KEY (`country_country_code`)
+    REFERENCES `country` (`country_code`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_country_has_language_language1`
+    FOREIGN KEY (`language_id`)
+    REFERENCES `language` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `language_resource`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `language_resource` ;
+
+CREATE TABLE IF NOT EXISTS `language_resource` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(200) NULL,
+  `resource_url` TEXT NULL,
+  `description` TEXT NULL,
+  `language_id` INT NOT NULL,
+  `added_by` INT NOT NULL,
+  `create_date` DATETIME NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_language_resource_language1_idx` (`language_id` ASC),
+  INDEX `fk_language_resource_user1_idx` (`added_by` ASC),
+  CONSTRAINT `fk_language_resource_language1`
+    FOREIGN KEY (`language_id`)
+    REFERENCES `language` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_language_resource_user1`
+    FOREIGN KEY (`added_by`)
+    REFERENCES `user` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -273,11 +378,21 @@ SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 
 -- -----------------------------------------------------
+-- Data for table `country`
+-- -----------------------------------------------------
+START TRANSACTION;
+USE `bilingualbuddiesdb`;
+INSERT INTO `country` (`country_code`, `country`) VALUES ('US', 'United States');
+
+COMMIT;
+
+
+-- -----------------------------------------------------
 -- Data for table `user`
 -- -----------------------------------------------------
 START TRANSACTION;
 USE `bilingualbuddiesdb`;
-INSERT INTO `user` (`id`, `email`, `username`, `password`, `date_created`, `last_login`, `enabled`, `role`, `sponsor`, `first_name`, `last_name`, `image_url`, `bio`) VALUES (1, 'admin@admin.com', 'admin', '$2a$10$4SMKDcs9jT18dbFxqtIqDeLEynC7MUrCEUbv1a/bhO.x9an9WGPvm', NULL, NULL, 1, 'admin', NULL, 'admin', 'admin', NULL, NULL);
+INSERT INTO `user` (`id`, `email`, `username`, `password`, `date_created`, `last_login`, `enabled`, `role`, `sponsor`, `first_name`, `last_name`, `image_url`, `bio`, `address_id`, `origin_country`) VALUES (1, 'admin@admin.com', 'admin', '$2a$10$4SMKDcs9jT18dbFxqtIqDeLEynC7MUrCEUbv1a/bhO.x9an9WGPvm', NULL, NULL, 1, 'admin', 0, 'admin', 'admin', NULL, NULL, NULL, 'US');
 
 COMMIT;
 
---
+++ JPABilingualBuddies/src/main/java/com/skilldistillery/bilingualbuddies/entities/Post.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.bilingualbuddies.entities;
+
+public class Post {
+
+}
