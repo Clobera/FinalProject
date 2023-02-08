5b35508033986d8a7cdf289c7d465178fc44f625
Carlos Lobera <carlosclobera@gmail.com>
2023-02-08 15:55:40 -0600

    fix compiler errors on CommentServiceImpl

--- BilingualBuddies/src/main/java/com/skilldistillery/bilingualbuddies/services/CommentServiceImpl.java
+++ BilingualBuddies/src/main/java/com/skilldistillery/bilingualbuddies/services/CommentServiceImpl.java
@@ -1,5 +1,69 @@
 package com.skilldistillery.bilingualbuddies.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.skilldistillery.bilingualbuddies.entities.Comment;
+import com.skilldistillery.bilingualbuddies.entities.Post;
+import com.skilldistillery.bilingualbuddies.repositories.CommentRepository;
+import com.skilldistillery.bilingualbuddies.repositories.PostRepository;
+
+@Service
 public class CommentServiceImpl implements CommentService {
+	
+	@Autowired
+	private CommentRepository commentRepo;
+	
+	@Autowired
+	private PostRepository postRepo;
+	
+	@Override
+	public List<Comment> findAllComments(int postId) {
+		List<Comment> comments = null;
+		if(commentRepo.findByPost_Id(postId).size() > 0) {
+			comments = commentRepo.findByPost_Id(postId);
+		}
+		
+		return comments;
+	}
+	
+	@Override
+	public Comment findById(int id) {
+		Optional<Comment> commentOpt = commentRepo.findById(id);
+		Comment comment = null;
+		if (commentOpt.isPresent()) {
+		  comment = commentOpt.get();
+		}
+		
+		return comment;
+	}
+	
+	@Override
+	public Comment createComment(int postId, Comment comment) {
+		
+		Optional<Post> postOpt = postRepo.findById(postId);
+		if (postOpt.isPresent()) {
+		  comment.setPost(postOpt.get());
+		  
+		  return commentRepo.saveAndFlush(comment);
+		}
+		
+		return null;
+	}
+	
+	@Override
+	public boolean deleteCommentById(int commentId) {
+		Comment deleteMe = findById(commentId);
+		boolean deleted = false;
+		if(deleteMe != null) {
+			commentRepo.delete(deleteMe);
+			deleted = true;
+		}
+		return deleted;
+	}
+	
 
 }
