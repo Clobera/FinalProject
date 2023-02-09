7d9fc0a54eb5b22236655b7479d729573b960061
Carlos Lobera <carlosclobera@gmail.com>
2023-02-09 11:08:04 -0600

    refactor destroy method in serviceImpl and method in AlertRepository

--- BilingualBuddies/src/main/java/com/skilldistillery/bilingualbuddies/services/AlertService.java
+++ BilingualBuddies/src/main/java/com/skilldistillery/bilingualbuddies/services/AlertService.java
@@ -1,9 +1,13 @@
 package com.skilldistillery.bilingualbuddies.services;
 
+import java.util.List;
+
 import com.skilldistillery.bilingualbuddies.entities.Alert;
 
 public interface AlertService {
 	public Alert findById(int alertId);
+	
+	public List<Alert> findByReceiver(int receiverId);
 
 	public Alert create(Alert alert);
 
