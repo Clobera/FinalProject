cd6b54c1c6955752373e2b4eb5bc3d7be2ca7ea9
Amcmike3 <amcmichael3@gmail.com>
2023-02-09 09:28:44 -0700

    finish registration html and binding

--- ngBilingualBuddies/src/app/app-routing.module.ts
+++ ngBilingualBuddies/src/app/app-routing.module.ts
@@ -7,11 +7,13 @@ import { LoginComponent } from './components/login/login.component';
 import { LogoutComponent } from './components/logout/logout.component';
 import { MygroupsComponent } from './components/mygroups/mygroups.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
   {path: 'account', component: AccountComponent},
   {path: 'mygroups', component: MygroupsComponent},
   {path: 'alerts', component: AlertsComponent},
