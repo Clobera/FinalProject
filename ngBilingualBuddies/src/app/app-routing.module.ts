bf87236150e4fbf1da2c101d4b39f1765b292a80
Amcmike3 <amcmichael3@gmail.com>
2023-02-09 22:33:46 -0700

    change routes

--- ngBilingualBuddies/src/app/app-routing.module.ts
+++ ngBilingualBuddies/src/app/app-routing.module.ts
@@ -21,7 +21,7 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'account', component: AccountComponent},
-  {path: 'mygroups', component: MygroupsComponent},
+  {path: 'myGroups', component: MygroupsComponent},
   {path: 'alerts', component: AlertsComponent},
   {path: 'logOut', component: LogoutComponent},
   {path: 'search', component: SearchComponent},
