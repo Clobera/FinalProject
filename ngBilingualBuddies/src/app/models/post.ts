3c9c07612d005918257142dd769ef350e7e4dae0
Lowell Belany <lrbelany@gmail.com>
2023-02-10 09:22:43 -0800

    All models completed

--- ngBilingualBuddies/src/app/models/post.ts
+++ ngBilingualBuddies/src/app/models/post.ts
@@ -1,2 +1,52 @@
+import { User } from "./user";
+
 export class Post {
+id: number;
+user: User;
+content: string;
+imageUrl: string;
+postDate: Date;
+enabled: boolean;
+comments: Array<Comment>;
+
+constructor(
+  id: number = 0,
+  user: User = new User(),
+  content: string = "",
+  imageUrl: string = "",
+  postDate: Date = new Date(),
+  enabled: boolean = false,
+  comments: Array<Comment> = []
+
+){
+
+  this.id = id;
+  this.user = user;
+  this.content = content;
+  this.imageUrl = imageUrl;
+  this.postDate = postDate;
+  this.enabled = enabled;
+  this.comments = comments;
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
