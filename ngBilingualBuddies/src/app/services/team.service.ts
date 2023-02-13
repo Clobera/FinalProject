575b0f6efbc3e4a7fcbcea431bb21e5b2bbea540
Sam Cassidy <samuelcassidy1999@gmail.com>
2023-02-13 11:26:32 -0700

    Stub team service

--- ngBilingualBuddies/src/app/services/team.service.ts
+++ ngBilingualBuddies/src/app/services/team.service.ts
@@ -12,7 +12,7 @@ export class TeamService {
 
 
   url = environment.baseUrl + "api/teams";
-  constructor(private httpClient : HttpClient, private auth : AuthService) { }
+  constructor(private http : HttpClient, private auth : AuthService) { }
 
   index(): Observable<Team[]> {
     let httpOptions = {
@@ -21,15 +21,70 @@ export class TeamService {
         'X-Requested-with': 'XMLHttpRequest',
       }
     };
-    return this.httpClient.get<Team[]>(this.url, httpOptions).pipe(
+    return this.http.get<Team[]>(this.url, httpOptions).pipe(
       catchError((err: any) => {
         console.log(err);
         return throwError(
           () => new Error(
-              'PostService.index(): error retrieving Posts: ' + err
+              ' Error retrieving Teams: ' + err
             )
         );
       })
     );
+  }
+
+  show(id: number): Observable<Team>{
+    return this.http.get<Team>(`${this.url}/${id}`, this.getHttpOptions()).pipe(
+      catchError((err: any) =>{
+        console.log(err);
+        return throwError(
+          () =>
+          new Error('Error retrieving Team: ' + err
+          )
+        )
+      })
+    )
+  }
+
+  create(team: Team): Observable<Team>{
+    return this.http.post<Team>(this.url, team, this.getHttpOptions()).pipe(
+      catchError((err: any) => {
+        console.log(err);
+        return throwError(
+          () => new Error('Error creating team: ' + err)
+        );
+      })
+    );
+  }
+
+  update(team: Team): Observable<Team>{
+    return this.http.put<Team>(`${this.url}/${team.id}`, team, this.getHttpOptions()).pipe(
+      catchError((err: any) =>{
+        console.log(err);
+        return throwError(
+          () => new Error('Error updating team: ' + err)
+        );
+      })
+    );
+  }
+
+  destroy(id: number): Observable<void>{
+    return this.http.delete<void>(`${this.url}/${id}`, this.getHttpOptions()).pipe(
+      catchError((err: any) =>{
+        console.log(err);
+        return throwError(
+          () => new Error('Error deleting team: ' + err)
+        )
+      })
+    )
+  }
+  getHttpOptions() {
+    let options = {
+      headers: {
+        Authorization: 'Basic ' + this.auth.getCredentials(),
+        'X-Requested-With': 'XMLHttpRequest',
+      },
+    };
+    return options;
   }
 }
