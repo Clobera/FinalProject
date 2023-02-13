7746f2af54065e393bfaf37f249fc1c7da55e4be
Amcmike3 <amcmichael3@gmail.com>
2023-02-13 13:31:00 -0700

    blah

--- ngBilingualBuddies/src/app/components/other-user/other-user.component.ts
+++ ngBilingualBuddies/src/app/components/other-user/other-user.component.ts
@@ -1,7 +1,10 @@
+import { AuthService } from 'src/app/services/auth.service';
+import { AlertService } from './../../services/alert.service';
 import { UserService } from 'src/app/services/user.service';
 import { ActivatedRoute } from '@angular/router';
 import { Component } from '@angular/core';
 import { User } from 'src/app/models/user';
+import { Alert } from 'src/app/models/alert';
 
 @Component({
   selector: 'app-other-user',
@@ -9,18 +12,35 @@ import { User } from 'src/app/models/user';
   styleUrls: ['./other-user.component.css']
 })
 export class OtherUserComponent {
-constructor(private route : ActivatedRoute, private userService : UserService) {}
+constructor(private route : ActivatedRoute, private userService : UserService, private alertService : AlertService, private auth : AuthService) {}
 
 username = '';
 user = new User();
 test = 0;
+friend = false;
+loggedInUser = new User();
 
 ngOnInit(){
   this.route.params.subscribe( params => this.username = params['id']);
   console.log(this.username);
   this.loadUser();
+  this.loadLoggedInUser();
 
 }
+
+loadLoggedInUser (){
+  this.auth.getLoggedInUser().subscribe({
+    next: (data) => {
+      this.loggedInUser = data;
+    },
+    error: (err) => {
+      console.log(
+        'OtherUserComponent.loadUser: Error loading user ' + err
+      );
+    }
+  })
+}
+
 loadUser(){
   this.userService.show(this.username).subscribe({
     next: (data) => {
@@ -28,11 +48,27 @@ loadUser(){
     },
     error: (err) => {
       console.log(
-        'SearchComponent.loadLanguages: Error loading languages ' + err
+        'OtherUserComponent.loadUser: Error loading user ' + err
       );
     }
   })
 }
+addFriend(){
+let alert = new Alert();
+alert.reciever = this.user;
+alert.sender =  this.loggedInUser;
+alert.content = "New Friend Request From: " + this.loggedInUser.firstName + " " + this.loggedInUser.lastName + "."
 
+  this.alertService.create(alert).subscribe({
+    next: (data) => {
+      this.friend = true;
+    },
+    error: (err) => {
+      console.log(
+        'OtherUserComponent.addFriend: Error creating new alert ' + err
+      );
+    }
+  })
+}
 
 }
