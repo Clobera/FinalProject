842ec9d659c43b2f7612d3464ca1e8b38d7bf556
Sam Cassidy <samuelcassidy1999@gmail.com>
2023-02-13 13:32:33 -0700

    Stub comment component and HTML

--- ngBilingualBuddies/src/app/app.module.ts
+++ ngBilingualBuddies/src/app/app.module.ts
@@ -29,6 +29,7 @@ import { NewsfeedComponent } from './components/newsfeed/newsfeed.component';
 import { TeamPipe } from './pipes/team.pipe';
 import { UserInCityPipe } from './pipes/user-in-city.pipe';
 import { OtherUserComponent } from './components/other-user/other-user.component';
+import { CommentComponent } from './components/comment/comment.component';
 
 @NgModule({
     declarations: [
@@ -56,6 +57,7 @@ import { OtherUserComponent } from './components/other-user/other-user.component
         TeamPipe,
         UserInCityPipe,
         OtherUserComponent,
+        CommentComponent,
 
     ],
     providers: [
